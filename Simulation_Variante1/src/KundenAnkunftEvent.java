717a9f8187eac258ab1755bc807a5c02a3d89864
abhogal <amrit.bhogal@hotmail.com>
2016-06-11 15:55:50 +0200

    Updated Simulation_Variante1

---
+++ Simulation_Variante1/src/KundenAnkunftEvent.java
@@ -0,0 +1,51 @@
+import desmoj.core.simulator.*;
+
+public class KundenAnkunftEvent extends Event<KundeEntity> {
+	
+	private final int MAXIMALE_LAENGE = 10;
+    private Schalter_Model meinModel;
+    
+
+    public KundenAnkunftEvent(Model owner, String name, boolean showInTrace) {
+        super(owner, name, showInTrace);
+
+        meinModel = (Schalter_Model) owner;
+
+    }
+
+    public void eventRoutine(KundeEntity kunde) {
+
+        // cast notwendig
+        //KundeEntity kunde =  who;
+
+    	if(meinModel.kundenReiheQueue.length() >= MAXIMALE_LAENGE) {
+    		meinModel.verloreneKunden.update();
+    	} else {
+    		// Kunde in Warteschlange
+            meinModel.kundenReiheQueue.insert(kunde);
+            sendTraceNote("Laenge der Kundenreihe: " + 
+                meinModel.kundenReiheQueue.length());
+    	}
+    	
+        // Schalter frei?
+        if (!meinModel.freieSchalterQueue.isEmpty()) {
+            // Schalter frei, von entsprechender WS holen
+            SchalterEntity schalter = meinModel.freieSchalterQueue.first();
+            // extra Entfernen von WS notwendig
+            meinModel.freieSchalterQueue.remove(schalter);
+            
+            // Schalter in entsprechende WS um Referenz nicht zu verlieren
+            meinModel.besetzteSchalterQueue.insert(schalter);
+            
+            // Kunden aus Kundenreihe um am Schalter bedient zu werden
+            // -> Referenz auf Kunden bereits vorhanden - kein first() n�tig!
+            meinModel.kundenReiheQueue.remove(kunde);
+            
+            // Bedienungsende Ereignis erzeugen
+            BedienEndeEvent bedienEnde = 
+                new BedienEndeEvent (meinModel, "Bedienung Ende", true);
+            // eintragen in Ereignisliste
+            bedienEnde.schedule(kunde, new TimeSpan(meinModel.getBedienZeit()));
+        }
+    }
+}
---
+++ Simulation_Variante1/src/Schalter_Model.java
@@ -0,0 +1,91 @@
+import desmoj.core.simulator.*;
+import desmoj.core.statistic.Count;
+import desmoj.core.dist.*;
+
+public class Schalter_Model extends Model {
+	
+	private final double ANKUNFTSZEIT_DURCHSCHNITT= 1;
+	private final double ANKUNFTSZEIT_ABWEICHUNG = 	0.35;
+	private final double BEDIENZEIT_UNTERGRENZE = 2;
+	private final double BEDIENZEIT_OBERGRENZE = 15;
+	
+	public Count verloreneKunden;
+	private ContDistNormal kundenAnkunftsZeit;
+
+    public double getKundenAnkunftsZeit() {
+    	// sample(): Returns the next negative exponential pseudo random number.
+    	System.out.println(kundenAnkunftsZeit.sample());
+	   return kundenAnkunftsZeit.sample();
+    }
+
+	private ContDistUniform bedienZeit;
+
+    public double getBedienZeit() {
+    	// sample(): Returns the next negative exponential pseudo random number.
+	   return bedienZeit.sample();
+    }
+
+    protected Queue<KundeEntity> kundenReiheQueue;
+
+	protected Queue<SchalterEntity> freieSchalterQueue;
+	
+	protected Queue<SchalterEntity> besetzteSchalterQueue;
+
+    public Schalter_Model(Model owner, String name, boolean showInReport, 
+                            boolean showIntrace) {
+    	super(owner, name, showInReport, showIntrace);
+    }
+
+    public String description() {
+    	return "Schalter1_e Modell (Ereignis orientiert):" +
+               "simuliert einen Bankschalter, wo ankommende Kunden zuerst in einer "+
+               "Warteschlange eingereiht werden. Wenn der Schalter frei ist, "+
+               "werden sie bedient.";
+    }	
+
+    public void doInitialSchedules() {
+        // erstes Kundenkreation-Ereignis erzeugen 
+        NeuerKundeEvent ersterKunde =
+		new NeuerKundeEvent(this, "Kundenkreation", true);
+         // erstes Kundenkreations-Ereignis in Ereignisliste eintragen
+         ersterKunde.schedule(new TimeSpan(getKundenAnkunftsZeit()));
+    }
+
+    public void init() {
+
+    	kundenAnkunftsZeit = 
+            new ContDistNormal(this, "Ankunftszeitintervall",ANKUNFTSZEIT_DURCHSCHNITT,ANKUNFTSZEIT_ABWEICHUNG,true,true);
+    	kundenAnkunftsZeit.setNonNegative(true);
+    	//kundenAnkunftsZeit.setSeed(1234567890);
+        bedienZeit = 
+            new ContDistUniform(this, "Bedienzeiten", BEDIENZEIT_UNTERGRENZE, BEDIENZEIT_UNTERGRENZE, true, true);	
+       	kundenReiheQueue = new Queue<KundeEntity>(this, "Kunden-Warteschlange",true, true);	
+    	freieSchalterQueue = new Queue<SchalterEntity>(this, "freie Schalter WS",true, true);
+    	verloreneKunden = new Count(this, "Verlorene Kunden", true, true);
+    	SchalterEntity schalter;
+    	for (int i = 1; i<=2; i++){
+    		schalter = new SchalterEntity(this, "Fahrkartenschalter", true);
+    		freieSchalterQueue.insert(schalter);
+    	} 
+        besetzteSchalterQueue = new Queue<SchalterEntity>(this, "besetzte Schalter WS", true, true);
+    }
+
+    public static void main(java.lang.String[] args) {
+
+    	Experiment schalterExperiment = 
+            new Experiment("Schalter1-ereignis");
+        Schalter_Model sch_e_model = 
+            new Schalter_Model(null, "Schalter Modell", true, true);  
+        
+        sch_e_model.connectToExperiment(schalterExperiment);
+
+        schalterExperiment.tracePeriod(new TimeInstant(0.0), new TimeInstant(60));
+        schalterExperiment.debugPeriod(new TimeInstant(0.0), new TimeInstant(60));
+        schalterExperiment.stop(new TimeInstant(600));
+
+        schalterExperiment.start(); 
+        schalterExperiment.report();
+        schalterExperiment.finish();
+	
+    }
+}
